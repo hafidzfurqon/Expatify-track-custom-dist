14f9873d2048f4ee6c92826f160702f8d1866f5a
hafidzfurqon <hafidzfurqonyet@gmail.com>
2026-03-23 10:53:45 +0700

    init theme 3

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,20 @@
-import { createRequestHandler } from "@react-router/node";
-import * as build from "../build/server/index.js";
+import { createRequestListener } from "@react-router/node";
 
-export default createRequestHandler({ build });
+let handler;
+
+export default async function (req, res) {
+  try {
+    if (!handler) {
+      const build = await import("../build/se`rver/index.js");
+      handler = createRequestListener({
+        build,
+        mode: "production",
+      });
+    }
+    return handler(req, res);
+  } catch (error) {
+    console.error("Serverless function error:", error);
+    res.statusCode = 500;
+    res.end("Internal Server Error: " + error.message);
+  }
+}
